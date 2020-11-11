fd08676e1c6b734037dfd042b3d13b15bae7695d
e <wangyisongku@163.com>
2020-11-11 21:46:52 +0800

    add_fanout

---
+++ MQ-Consumer/src/main/java/com/wys/exchange/fanout/FanoutReceiverC.java
@@ -0,0 +1,28 @@
+package com.wys.exchange.fanout;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @ProjectName: SpringCloud
+ * @Package: com.wys.exchange.fanout
+ * @ClassName: FanoutReceiverC
+ * @Author: wangy
+ * @Description: ${description}
+ * @Date: 2020/11/11 21:41
+ * @Version: 1.0
+ */
+
+@Component
+@RabbitListener(queues = "fanout.C")
+public class FanoutReceiverC {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("FanoutReceiverC消费者收到消息  : " +testMessage.toString());
+    }
+
+}
